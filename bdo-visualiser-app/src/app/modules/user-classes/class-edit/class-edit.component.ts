3a1042d5a86e8eff032539b84fccf8fba44a0140
Stephen Devenney <33627847+StephenDevenney@users.noreply.github.com>
2021-07-03 14:15:40 +0100

    class edit UI changes, fix for getting current combat builds

--- bdo-visualiser-app/src/app/modules/user-classes/class-edit/class-edit.component.ts
+++ bdo-visualiser-app/src/app/modules/user-classes/class-edit/class-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ClassEditViewModel } from 'src/server/shared/viewModels/userClassViewModel';
+import { BuildsViewModel, ClassEditViewModel, GearViewModel } from 'src/server/shared/viewModels/userClassViewModel';
 import { BaseComponent } from '../../../shared/components/base.component';
 import { UserClassesService } from '../user-classes.service';
 
@@ -11,6 +11,7 @@ import { UserClassesService } from '../user-classes.service';
 export class ClassEditPageComponent extends BaseComponent implements OnInit  {
   public isLoaded: boolean = false;
   public classId: number = 0;
+  public gearBuilds: BuildsViewModel = new BuildsViewModel();
 
   constructor(private injector: Injector,
               private userClassService: UserClassesService,
@@ -26,6 +27,7 @@ export class ClassEditPageComponent extends BaseComponent implements OnInit  {
       if(this.classId > 0) {
         this.userClassService.getClassEditData(this.classId).then((res: ClassEditViewModel) => {
           console.log(res);
+          this.gearBuilds = res.builds;
           this.isLoaded = true;
         }).catch(() => {
     
